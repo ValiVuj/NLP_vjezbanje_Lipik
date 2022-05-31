e9c6bd7e52d000b959687d3f6ba5928042ecfefc
ValiVuj <82405158+ValiVuj@users.noreply.github.com>
2022-05-31 11:24:01 +0200

    Add files via upload

---
+++ udemy_vjezba.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+dict1={}
+dict1["apple"] = "Apple is a fruit"
+dict1["orage"] = "orange is a fruit"
+
+print(dict1)
+
+
+y = np.zeros((2, 3, 4))
+print(y)
+
+x = np.array([1, 2, 3])
+print(x)
